3a083d2ce83f81443875f81a5373e660dceb6595
Ghostovich <gusarov_konstant@mail.ru>
2022-06-14 21:22:21 +0300

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/Components/Header.jsx 	new file:   src/Components/Navbar.jsx 	new file:   src/Components/Profile.jsx 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,35 @@
+import React from 'react'; 
+
+const Profile = () => {
+  return (
+    <div className='content'>
+
+    <div>
+      <img alt="r" src="https://imgv3.fotor.com/images/side/sideimage-one-tap-enhance.jpg" />
+    </div>
+
+    <div>
+      ava+discription
+    </div>
+
+    <div>
+      my posts
+      <div>
+        new post
+      </div>
+    </div>
+
+    <div>
+      <div>
+        post1
+      </div>
+      <div>
+        post2
+      </div>
+    </div>
+
+  </div>
+);
+}
+
+export default Profile;
