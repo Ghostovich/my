e3983ad721a0bd0ffdb2550f5d1e1f8444f32f47
Ghostovich <gusarov_konstant@mail.ru>
2022-06-26 23:51:19 +0300

    19

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
-/*import Profile from './Components/Profile/Profile';*/
+import Profile from './Components/Profile/Profile';
 import Dialogs from "./Components/Dialogs/Dialogs";
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import News from "./Components/News/News";
+import Music from "./Components/Music/Music";
+import Settings from "./Components/Setttings/Settings";
 
 const App = () => {
     return (
-
+<BrowserRouter>
         <div className='app-wrapper'>
 
             <Header/>
             <Navbar/>
-            {/*<Profile />*/}
+
             <div className="app-wrapper-content">
-                <Dialogs />
-            </div>
+                <Routes>
+                <Route path="/Dialogs" element={<Dialogs/>} />
+                <Route path='/Profile' element={<Profile/>} />
+                    <Route path='/News' element={<News/>} />
+                    <Route path='/Music' element={<Music/>} />
+                    <Route path='/Settings' element={<Settings/>} />
+                </Routes>
+             </div>
 
 
         </div>
-    );
+</BrowserRouter>
+    )
 }
 
 export default App;
