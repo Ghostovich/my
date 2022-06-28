3b6e09f4a04ea8dc1b681ea86412233bdf0fbe68
Ghostovich <gusarov_konstant@mail.ru>
2022-06-29 01:05:08 +0300

    21

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-/*import s from "./Dialogs.module.css";*/
+import s from "./Dialogs.module.css"
+
 
 const Dialogs =() =>{
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+
+           <div className={s.dialogsItems}>
+
+               <div className={s.dialog + " " + s.active}>Phil</div>
+               <div className={s.dialog}>Anton</div>
+               <div className={s.dialog}>Petya</div>
+               <div className={s.dialog}>Vasya</div>
+
+           </div>
+
+           <div className={s.messages}>
+
+               <div className={s.message}>hi</div>
+               <div className={s.message}>men</div>
+               <div className={s.message}>what`s up</div>
+
+           </div>
         </div>
     )
 }
