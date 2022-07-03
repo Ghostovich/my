62bbe69dac95876a7a777d7a97da92daeeb776fd
Ghostovich <gusarov_konstant@mail.ru>
2022-07-03 17:35:20 +0300

    24

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -4,39 +4,59 @@ import {NavLink} from "react-router-dom"
 
 const Dialog = (props) => {
 
-    let path="/Dialogs/" + props.id
+
+    let path = "/Dialogs/" + props.id
 
     return (
-        <div >
-        <NavLink to={path} className= { dialogData => dialogData.isActive ? s.active : s.dialog }>{props.name}</NavLink>
+        <div>
+            <NavLink to={path}
+                     className={dialogData => dialogData.isActive ? s.active : s.dialog}>{props.name}</NavLink>
         </div>
     )
 }
 
-const Message=(props)=>{
-return (
-    <div className={s.message}>{props.message}</div>
+const Message = (props) => {
+    return (
+        <div className={s.message}>{props.message}</div>
 
-)
+    )
 }
 
 const Dialogs = () => {
+
+    let DItem = [
+        {id: 1, name: "Phil"},
+        {id: 2, name: "Anton"},
+        {id: 3, name: "Petya"},
+        {id: 4, name: "Vasya"}
+
+    ];
+
+    let Mess = [
+        {id: 1, mess: "Hi"},
+        {id: 2, mess: "Men"},
+        {id: 3, mess: "What`s up"}
+
+
+    ];
+
+
     return (
         <div className={s.dialogs}>
 
             <div className={s.dialogsItems}>
-                <Dialog name="Phil" id="1"/>
-                <Dialog name="Anton" id="2"/>
-                <Dialog name="Petya" id="3"/>
-                <Dialog name="Vasya" id="4"/>
+                <Dialog name={DItem[0].name} id={DItem[0].id}/>
+                <Dialog name={DItem[1].name} id={DItem[1].id}/>
+                <Dialog name={DItem[2].name} id={DItem[2].id}/>
+                <Dialog name={DItem[3].name} id={DItem[3].id}/>
 
             </div>
 
             <div className={s.messages}>
 
-                <Message message="hi"/>
-                <Message message="men"/>
-                <Message message="what`s up?"/>
+                <Message message={Mess[0].mess}/>
+                <Message message={Mess[1].mess}/>
+                <Message message={Mess[2].mess}/>
 
             </div>
         </div>
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import s from "./MyPosts.module.css";
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let Posts = [
+        {id:1, message: "Come on!", Like:2},
+        {id:2, message: "Eee", Like: 4}
+    ]
+
   return (
     <div className={s.item}>
       <div>
@@ -20,10 +26,12 @@ const MyPosts = () => {
       </div>
 
         <div className={s.post}>
-            <Post Like="2" Message="Come on!"/>
+            <Post Like={Posts[0].Like} Message={Posts[0].message}/>
         </div>
 
-
+        <div className={s.post}>
+            <Post Like={Posts[1].Like} Message={Posts[1].message}/>
+        </div>
 
 
     </div>
